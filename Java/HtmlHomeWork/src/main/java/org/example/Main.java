f6a039faf0447203b22e5928c9f138984a18ae3c
Alexey <110184110+Polyakov1@users.noreply.github.com>
2023-12-11 20:59:42 +0500

    Commit HomeWorkHtml

---
+++ Java/HtmlHomeWork/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+// Скачать и настроить редактор кода, если этого не сделали на семинаре
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ Java/ThrowExcption/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       //ClassCastException
+        //StackOverflowError
+        //IOError
+        //IOException
+        //NullPointerException
+        //ConcurrentModificationException
+        //IllegalArgumentException
+        //EOFException
+        //ClassNotFoundException
+        //ThreadDeath
+        ClassCastException mew = new ClassCastException(" asd");
+    }
+}
